6d87294b1765b03510c4b2fec994f8f3cadc0ed4
Lucas de Castro <lucas.castro@vagas.com.br>
2018-09-14 20:59:24 -0300

    Create interceptor with axios to set token

--- src/requests.js
+++ src/requests.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 
+const createAxiosInstance = () => {
+  const instace = axios.create({
+    baseURL: BASE_URL,
+  });
+  instace.interceptors.request.use((config) => {
+    const token = localStorage.getItem("access_token")
+
+    if (token !== null) {
+      config.headers.Authorization = token;
+    }
+
+    return config;
+  }, function (err) {
+    return Promise.reject(err);
+  });
+  return instace
+}
+
 export const BASE_URL = "https://api.github.com/repos/";
-export const instace = axios.create({
-  baseURL: BASE_URL,
-  headers: { Authorization: `token ${localStorage.getItem("access_token")}` }
-});
+export const instace = createAxiosInstance()
 
 export function getIssues(repo) {
   return instace.get(`${repo}/vagas/issues`);
