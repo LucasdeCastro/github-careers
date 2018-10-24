c62e8cb537c5b2983a606693d98642f0aebdbe5f
Lucas Castro <lucascastro102@gmail.com>
2018-10-23 22:48:09 -0300

    Add react-ga

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import ReactGA from "react-ga";
 import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGhost } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles/main.css";
 import App from "./cotainers";
+
+ReactGA.initialize("UA-128061632-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 library.add(faGhost);
 
